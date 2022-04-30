e3c22273f0fe37ba110e1218ed3b96420c85c58f
VanessaGiannoni <vanessa.o.giannoni@gmail.com>
2022-04-29 21:19:36 -0300

    feature: expo location react native

---
+++ locationReact/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+
+export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [altitude, setAltitude] = useState(null);
+  const [speed, setSpeed] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        setErrorMsg('Permissão para acessar o local negada!');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+      setLatitude(location.coords.latitude);
+      setLongitude(location.coords.longitude);
+      setAltitude(location.coords.altitude);
+      setSpeed(location.coords.speed);
+    })();
+
+    let text = 'Aguardando...';
+    if (errorMsg) {
+      text = errorMsg
+    } else if (location) {
+      text = JSON.stringify(location)
+    }
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ marginTop: 50 }}> Latitude: {latitude} </Text>
+      <Text style={{ marginTop: 50 }}> Longitude: {longitude} </Text>
+      <Text style={{ marginTop: 50 }}> Altitude: {altitude} </Text>
+      <Text style={{ marginTop: 50 }}> Velocidade: {speed} </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
